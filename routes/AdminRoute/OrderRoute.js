ec84e787115461169b3f649392f4a4920f29905e
sanket-vanani <sankethv8509@gmail.com>
2020-07-01 17:04:33 +0530

    ADD:Craete Order

--- routes/AdminRoute/OrderRoute.js
+++ routes/AdminRoute/OrderRoute.js
@@ -1,13 +1,125 @@
 var connection = require('./../../config');
 const express = require('express');
 const router = express.Router();
+const format = require('dateformat');
 
-router.get('/',(req,res)=>{
-    res.render('order');
+router.get('/', (req, res) => {
+    connection.query('SELECT * FROM ProductTbl', function (error, results, fields) {
+        if (error) {
+            res.redirect('/errpage');
+        }
+        else {
+            var op = {
+                flag: 0,
+                success: "true",
+                status: 200,
+                data: results,
+                message: "Redirected"
+            }
+        }
+        res.render('order', { results });
+    });
 })
 
-router.post('/add',(req,res)=>{
-    let cmb = req.body.cmbtest;
-    res.send(cmb);
+router.get('/list',(req,res)=>{
+    connection.query('SELECT o.OrderId,o.NODWarranty,o.CustomerId,u.UserName,u.ContactNo,o.CreatedAt FROM OrderTbl o,UserTbl u WHERE o.CustomerId=u.UserId AND u.RoleId=1',(error, results, fields)=>{
+        if(error)
+        {
+            res.redirect('/error')
+        }
+        else
+        {
+            var i
+            var cnt = 1
+            var newobj = [];
+            for (i = 0; i < results.length; i++) {
+
+                var tmpdate = results[i].CreatedAt;
+                var dt = format(tmpdate,'dd-mm-yyyy');
+                newobj.push({
+                    'OrderId': results[i].OrderId,
+                    'NODWarranty': results[i].NODWarranty,
+                    'CustomerId': results[i].CustomerId,
+                    'UserName': results[i].UserName,
+                    'ContactNo': results[i].ContactNo,
+                    'CreatedAt': dt
+                })
+                cnt += 1
+            }
+            if (cnt > results.length) {
+                var op = {
+                    flag: 0,
+                    success: "true",
+                    status: 200,
+                    data: newobj,
+                    message: "Redirected"
+                }
+                // res.render('inquiry', { op });
+                res.send(newobj)
+            }
+        }
+    })
+})
+
+router.get('/pr', (req, res) => {
+    connection.query('SELECT * FROM ProductTbl', function (error, results, fields) {
+        if (error) {
+            res.redirect('/errpage');
+        }
+        else {
+            res.send(results);
+        }
+    });
+})
+
+router.post('/add', (req, res) => {
+    let customerid = req.body.cmbCustomer;
+    let productid = req.body.txtProductName;
+    let warranty = req.body.txtWarranty;
+    // console.log(warranty,customerid,productid);
+    // for(var i=0;i<productid.length; i++)
+    // {
+    //     connection.query("")
+    // }
+    var oid
+    connection.query('INSERT INTO OrderTbl(CustomerId,NODWarranty,WarrantyExpired) VALUES (?,?,?)', [customerid, warranty, 0], (error, results, fields) => {
+        if (error) {
+            res.redirect('/errpage');
+        }
+        else {
+            // console.log(results.insertId);
+            oid = results.insertId;
+            var flag = productid.length
+            var i
+            connection.query('INSERT INTO JobTbl(CustomerId,OrderId,JobStatus,AssignedTo) VALUES(?,?,?,?)', [customerid, oid, 0, 0], (error, results, fields) => {
+                if (error) {
+                    res.redirect('/errpage');
+                }
+                else {
+                    for (i = 0; i < productid.length; i++) {
+                        // console.log(i);
+                        console.log(productid[i])
+                        connection.query('INSERT INTO OrderDetailsTbl(OrderId,ProductId) VALUES(?,?)', [oid, productid[i]], (error, results, fields) => {
+                            if (error) {
+                                res.redirect('/errpage');
+                            }
+                            else {
+                                //Do nothing
+                                // console.log(i);
+                            }
+                            flag = i
+                        })
+                    }
+                    if (i == flag) {
+                        res.redirect('/order');
+                    }
+                    else {
+                        console.log("no" + flag);
+                    }
+                }
+            })
+        }
+    })
+    // res.send(cmb);
 })
 module.exports = router;
