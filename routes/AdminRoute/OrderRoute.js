5902c7ddc8c0e34c682da1795fb8d70d9b334d08
sanket-vanani <sankethv8509@gmail.com>
2020-07-10 16:41:29 +0530

    order route change

--- routes/AdminRoute/OrderRoute.js
+++ routes/AdminRoute/OrderRoute.js
@@ -60,15 +60,12 @@ router.get('/list', (req, res) => {
                     message: "Redirected"
                 }
                 res.render('orderlist', { op });
-                // res.send(newobj)
             }
         }
     })
 })
 
 router.post('/pr', (req, res) => {
-
-    // console.log("Method called");
     connection.query('SELECT * FROM ProductTbl', function (error, results, fields) {
         if (error) {
             res.redirect('/errpage');
@@ -92,7 +89,6 @@ router.post('/add', (req, res) => {
             res.redirect('/errpage');
         }
         else {
-            // console.log(results.insertId);
             oid = results.insertId;
             var flag = productid.length
             var i
@@ -102,15 +98,11 @@ router.post('/add', (req, res) => {
                 }
                 else {
                     for (i = 0; i < productid.length; i++) {
-                        // console.log(i);
-                        // console.log(productid[i])
                         connection.query('INSERT INTO OrderDetailsTbl(OrderId,ProductId) VALUES(?,?)', [oid, productid[i]], (error, results, fields) => {
                             if (error) {
                                 res.redirect('/errpage');
                             }
                             else {
-                                //Do nothing
-                                // console.log(i);
                             }
                             flag = i
                         })
@@ -141,19 +133,11 @@ router.get('/detail/:oid',(req,res)=>{
     let orderid = req.params.oid;
     connection.query('SELECT o.OrderId,u.UserName,a.AreaName,s.ServiceName,w.TypeName,o.NODWarranty,o.CreatedAt FROM OrderTbl o,ServiceTbl s, WallpaperTypeTbl w, UserTbl u, AreaTbl a where u.AreaId = a.AreaId and u.UserId = o.CustomerId and o.ServiceId= s.ServiceId and o.TypeId=w.TypeId and o.OrderId=?', [orderid], (error, results, fields) => {
         if (error) {
-            // return res.json({
-            //     status: false,
-            //     message: err.message
-            // }).end()
             res.redirect('/errpage');
         }
         else {
             connection.query('SELECT p.ProductId,c.CategoryName,p.ProductTitle,p.Price,p.Details,p.ProductImg FROM ProductTbl p,CategoryTbl c, OrderDetailsTbl od where p.CategoryId=c.CategoryId AND od.ProductId=p.ProductId AND od.OrderId=?', [orderid], (error, products, fields) => {
                 if (error) {
-                    // return res.json({
-                    //     status: false,
-                    //     message: err.message
-                    // }).end()
                     res.redirect('/errpage')
                 }
                 else {
@@ -164,13 +148,6 @@ router.get('/detail/:oid',(req,res)=>{
                     var op = [];
                     if (products.length > 0) 
                     {
-                        // var i = 0;
-                        // for (i = 0; i < products.length; i++) {
-                        //     var img = products[i].ProductImg;
-                        //     if (img !== undefined) {
-                        //         products[i].ProductImg = imagepath + '/' + img;
-                        //     }
-                        // }
                         op.push({
                             'OrderId': results[0].OrderId,
                             'UserName': results[0].UserName,
@@ -181,11 +158,6 @@ router.get('/detail/:oid',(req,res)=>{
                             'product': products,
                             'CreatedAt': dt
                         })
-                        // return res.json({
-                        //     status: true,
-                        //     message: "Order Details are found",
-                        //     categories: op
-                        // })
                         res.render('orderview',{op});
                     }
                 }
@@ -193,35 +165,6 @@ router.get('/detail/:oid',(req,res)=>{
         }
     })
 })
-
-// router.get('/detail/:oid', (req, res) => {
-//     // console.log(req.params.oid);
-//     let orderid = req.params.oid;
-//     //getting product image SELECT p.* FROM ProductTbl p, OrderDetailsTbl od where od.ProductId=p.ProductId AND od.OrderId=2 
-//     //Done let sql = 'SELECT o.OrderId,u.UserName,s.ServiceName,w.TypeName,o.NODWarranty,o.CreatedAt FROM OrderTbl o,ServiceTbl s, WallpaperTypeTbl w, UserTbl u where u.UserId = o.CustomerId and o.ServiceId= s.ServiceId and o.TypeId=w.TypeId and o.OrderId=2';
-//     connection.query('SELECT o.OrderId,u.UserName,a.AreaName,s.ServiceName,w.TypeName,o.NODWarranty,o.CreatedAt FROM OrderTbl o,ServiceTbl s, WallpaperTypeTbl w, UserTbl u, AreaTbl a where u.AreaId = a.AreaId and u.UserId = o.CustomerId and o.ServiceId= s.ServiceId and o.TypeId=w.TypeId and o.OrderId=?', [orderid], (error, results, fields) => {
-//         if (error) {
-//             res.redirect('/errpage');
-//         } else {
-//             var op = [];
-
-//             var tmpdate = results[0].CreatedAt;
-//             var dt = format(tmpdate, 'dd-mm-yyyy');
-//             // op.push({
-//             //     'OrderId': results[0].OrderId,
-//             //     'UserName': results[0].UserName,
-//             //     'AreaName': results[0].AreaName,
-//             //     'ServiceName':results[0].ServiceName,
-//             //     'TypeName':results[0].TypeName,
-//             //     'NODWarranty': results[0].NODWarranty,
-//             //     'CreatedAt': dt
-//             // })
-//             results[0].CreatedAt = dt;
-//             op.push(results)
-//             res.render('orderview', op);
-//         }
-//     })
-// })
 
 router.post('/productimg', (req, res) => {
     let oid = req.body.OrderId;
