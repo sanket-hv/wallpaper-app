95cf3ecdeb0d942841dd84f1bfe70f063abc1f76
sanket-vanani <sankethv8509@gmail.com>
2020-12-05 10:03:24 +0530

    truncated wallpaperDB added

--- db/lnmwallpaperDB.sql
+++ db/lnmwallpaperDB.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 11, 2020 at 06:51 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
+-- Host: localhost:3306
+-- Generation Time: Dec 05, 2020 at 12:32 PM
+-- Server version: 10.2.36-MariaDB
+-- PHP Version: 7.3.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `wallpaperDB`
+-- Database: `lnmwallpaperDB`
 --
 
 -- --------------------------------------------------------
@@ -39,7 +38,8 @@ CREATE TABLE `AdminTbl` (
 --
 
 INSERT INTO `AdminTbl` (`AdminId`, `Email`, `Password`) VALUES
-(1, 'admin@gmail.com', '0e7517141fb53f21ee439b355b5a1d0a');
+(1, 'admin@gmail.com', '0e7517141fb53f21ee439b355b5a1d0a'),
+(2, 'davidmookmhs@gmail.com', 'b78d3dba718cfa46f79ce02120ed57fb');
 
 -- --------------------------------------------------------
 
@@ -58,7 +58,7 @@ CREATE TABLE `AreaTbl` (
 --
 
 INSERT INTO `AreaTbl` (`AreaId`, `AreaName`, `IsActive`) VALUES
-(1, 'Area 1', 0);
+(1, 'Surat', 0);
 
 -- --------------------------------------------------------
 
@@ -73,13 +73,6 @@ CREATE TABLE `CategoryTbl` (
   `IsActive` int(11) NOT NULL COMMENT '0-Show, 1-Hidden'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `CategoryTbl`
---
-
-INSERT INTO `CategoryTbl` (`CategoryId`, `CategoryName`, `Img`, `IsActive`) VALUES
-(1, 'Nature', '1596303699361scenic-nature-landscape-path-near-lake-forest-path-tunnel-trees-near-lake-scenic-nature-autumn-landscape-panorama-view-115358410.jpg', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -90,11 +83,11 @@ CREATE TABLE `ComplaintTbl` (
   `ComplaintId` int(11) NOT NULL,
   `OrderId` int(11) NOT NULL,
   `ComplaintImg` text NOT NULL,
-  `Remarks` text,
+  `Remarks` text DEFAULT NULL,
   `ComplaintStatus` int(11) DEFAULT NULL COMMENT '0-Pending, 1-In Progress, 2-Completed',
   `AssignedTo` int(11) NOT NULL COMMENT 'Reference to the installer Id',
-  `CreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -107,8 +100,8 @@ CREATE TABLE `GalleryTbl` (
   `GalleryId` int(11) NOT NULL,
   `JobId` int(11) NOT NULL,
   `GalleryImg` text NOT NULL COMMENT 'This is the image after installation',
-  `Hidden` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0-Hidden, 1-Show',
-  `CreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `Hidden` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0-Hidden, 1-Show',
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for Gallery and Installation Image';
 
 -- --------------------------------------------------------
@@ -124,7 +117,7 @@ CREATE TABLE `InquiryTbl` (
   `ContactNo` text NOT NULL,
   `EmailId` text NOT NULL,
   `Remarks` varchar(50) NOT NULL,
-  `IsAttended` int(11) NOT NULL DEFAULT '0' COMMENT '0- Not Attended, 1- Attended'
+  `IsAttended` int(11) NOT NULL DEFAULT 0 COMMENT '0- Not Attended, 1- Attended'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -136,18 +129,11 @@ CREATE TABLE `InquiryTbl` (
 CREATE TABLE `JobTbl` (
   `JobId` int(11) NOT NULL,
   `OrderId` int(11) NOT NULL,
-  `JobStatus` int(11) NOT NULL DEFAULT '0' COMMENT '0-Pending,1-In Progeress,2-Completed',
-  `AssignedTo` int(11) NOT NULL DEFAULT '0' COMMENT 'Reference to Installer Id',
-  `CreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `JobStatus` int(11) NOT NULL DEFAULT 0 COMMENT '0-Pending,1-In Progeress,2-Completed',
+  `AssignedTo` int(11) NOT NULL DEFAULT 0 COMMENT 'Reference to Installer Id',
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `UpdatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `JobTbl`
---
-
-INSERT INTO `JobTbl` (`JobId`, `OrderId`, `JobStatus`, `AssignedTo`, `CreatedAt`, `UpdatedAt`) VALUES
-(3, 1, 1, 1, '2020-08-11 04:39:00', '2020-08-11 04:39:27');
 
 -- --------------------------------------------------------
 
@@ -178,15 +164,8 @@ CREATE TABLE `OrderDetailsTbl` (
   `OrderDetailsId` int(11) NOT NULL,
   `OrderId` int(11) NOT NULL,
   `ProductId` int(11) NOT NULL,
-  `CreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `OrderDetailsTbl`
---
-
-INSERT INTO `OrderDetailsTbl` (`OrderDetailsId`, `OrderId`, `ProductId`, `CreatedAt`) VALUES
-(1, 1, 1, '2020-08-02 10:29:46');
 
 -- --------------------------------------------------------
 
@@ -200,17 +179,11 @@ CREATE TABLE `OrderTbl` (
   `ServiceId` int(11) NOT NULL,
   `TypeId` int(11) NOT NULL,
   `NODWarranty` int(11) NOT NULL,
-  `WarrantyExpired` int(11) NOT NULL DEFAULT '0' COMMENT '0-No, 1-Yes',
-  `CreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `UpdatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+  `WallDimension` varchar(255) NOT NULL DEFAULT '0',
+  `WarrantyExpired` int(11) NOT NULL DEFAULT 0 COMMENT '0-No, 1-Yes',
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `UpdatedAt` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `OrderTbl`
---
-
-INSERT INTO `OrderTbl` (`OrderId`, `CustomerId`, `ServiceId`, `TypeId`, `NODWarranty`, `WarrantyExpired`, `CreatedAt`, `UpdatedAt`) VALUES
-(1, 1, 8, 2, 30, 0, '2020-08-02 10:29:46', '2020-08-02 10:29:46');
 
 -- --------------------------------------------------------
 
@@ -222,23 +195,13 @@ CREATE TABLE `ProductTbl` (
   `ProductId` int(11) NOT NULL,
   `CategoryId` int(11) NOT NULL COMMENT 'Reference to category',
   `ProductTitle` varchar(50) NOT NULL,
-  `Price` varchar(10) DEFAULT NULL,
-  `Details` text,
-  `IsActive` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0-Show, 1-Hidden',
+  `Price` varchar(50) DEFAULT NULL,
+  `Details` text DEFAULT NULL,
+  `IsActive` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0-Show, 1-Hidden',
   `ProductImg` text NOT NULL,
-  `CreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `UpdatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `UpdatedAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ProductTbl`
---
-
-INSERT INTO `ProductTbl` (`ProductId`, `CategoryId`, `ProductTitle`, `Price`, `Details`, `IsActive`, `ProductImg`, `CreatedAt`, `UpdatedAt`) VALUES
-(1, 1, 'Nature Product', '500', 'this is the details okay?', 0, '1596303719849scenic-nature-landscape-path-near-lake-forest-path-tunnel-trees-near-lake-scenic-nature-autumn-landscape-panorama-view-115358410.jpg', '2020-08-01 18:20:15', '2020-08-01 17:41:59'),
-(2, 1, 'cnfgb', '5', 'vnbcvngf', 0, '1597118488299Screen Shot 2020-08-08 at 12.38.32.png', '2020-08-11 04:01:28', '2020-08-11 04:01:28'),
-(3, 1, 'fhjnhfj', '5', 'gbnvbn', 0, '1597118504573IMG_20200714_141711.jpg', '2020-08-11 04:01:44', '2020-08-11 04:01:44'),
-(4, 1, 'vhnvhm', '5.1', 'nfvbn vbn', 0, '1597118575151IMG_20200714_141711.jpg', '2020-08-11 04:02:55', '2020-08-11 04:02:55');
 
 -- --------------------------------------------------------
 
@@ -300,16 +263,9 @@ CREATE TABLE `UserTbl` (
   `Password` text NOT NULL,
   `AreaId` int(11) NOT NULL,
   `RoleId` int(11) NOT NULL COMMENT '1-Customer, 2- Installer',
-  `CreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `UpdatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `UpdatedAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `UserTbl`
---
-
-INSERT INTO `UserTbl` (`UserId`, `UserName`, `Email`, `ContactNo`, `Address`, `Password`, `AreaId`, `RoleId`, `CreatedAt`, `UpdatedAt`) VALUES
-(1, 'customer1', 'customer1@gmail.com', '8780015120', 'refsgfdgdfgsf       ', 'customer1', 1, 1, '2020-08-02 10:29:11', '2020-08-02 10:29:11');
 
 -- --------------------------------------------------------
 
@@ -331,7 +287,7 @@ INSERT INTO `WallpaperTypeTbl` (`TypeId`, `TypeName`) VALUES
 (2, 'Korean Wallpaper'),
 (3, 'Mural Wallpaper'),
 (4, 'Touch Up Service'),
-(5, '#M Sticker'),
+(5, '3M Sticker'),
 (6, 'Canvas'),
 (7, 'Roller Blinds');
 
@@ -347,13 +303,6 @@ CREATE TABLE `WarrantyTbl` (
   `WarrantyValue` int(11) NOT NULL COMMENT 'Warrany value in days.',
   `IsActive` int(11) NOT NULL COMMENT '0-Show, 1-Hidden'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `WarrantyTbl`
---
-
-INSERT INTO `WarrantyTbl` (`WarrantyId`, `WarrantyName`, `WarrantyValue`, `IsActive`) VALUES
-(1, '1 Month', 30, 0);
 
 --
 -- Indexes for dumped tables
@@ -463,7 +412,7 @@ ALTER TABLE `WarrantyTbl`
 -- AUTO_INCREMENT for table `AdminTbl`
 --
 ALTER TABLE `AdminTbl`
-  MODIFY `AdminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `AdminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `AreaTbl`
@@ -475,7 +424,7 @@ ALTER TABLE `AreaTbl`
 -- AUTO_INCREMENT for table `CategoryTbl`
 --
 ALTER TABLE `CategoryTbl`
-  MODIFY `CategoryId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `CategoryId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `ComplaintTbl`
@@ -499,7 +448,7 @@ ALTER TABLE `InquiryTbl`
 -- AUTO_INCREMENT for table `JobTbl`
 --
 ALTER TABLE `JobTbl`
-  MODIFY `JobId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `JobId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `OfferTbl`
@@ -511,19 +460,19 @@ ALTER TABLE `OfferTbl`
 -- AUTO_INCREMENT for table `OrderDetailsTbl`
 --
 ALTER TABLE `OrderDetailsTbl`
-  MODIFY `OrderDetailsId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `OrderDetailsId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `OrderTbl`
 --
 ALTER TABLE `OrderTbl`
-  MODIFY `OrderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `OrderId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `ProductTbl`
 --
 ALTER TABLE `ProductTbl`
-  MODIFY `ProductId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ProductId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `RoleTbl`
@@ -541,7 +490,7 @@ ALTER TABLE `ServiceTbl`
 -- AUTO_INCREMENT for table `UserTbl`
 --
 ALTER TABLE `UserTbl`
-  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `WallpaperTypeTbl`
@@ -553,7 +502,7 @@ ALTER TABLE `WallpaperTypeTbl`
 -- AUTO_INCREMENT for table `WarrantyTbl`
 --
 ALTER TABLE `WarrantyTbl`
-  MODIFY `WarrantyId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `WarrantyId` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
